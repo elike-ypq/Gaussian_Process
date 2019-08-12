d589c8e9650fbf6cf938b64285b7cfc9695e1698
elike-ypq <2413759410@qq.com>
2019-08-12 09:32:20 +0800

    some covariance successful

--- GPR_kernel.m
+++ GPR_kernel.m
@@ -1,6 +1,9 @@
-function y=GPR_kernel(a,b,L)
+function y=GPR_kernel(a,b,L,sigma_n2)
 if nargin==2
-    L=1;
+    L=1;sigma_n2=1e-8;
+end
+if nargin==3
+    sigma_n2=1e-8;
 end
 sw_1=0;
 N1=length(a);
@@ -14,5 +17,5 @@ a=M_1*a;
 M_2=L.*eye(N2);
 b=b*M_2;
 r2=(a.^2)*ones(1,N2)+ones(N1,1)*(b.^2)-2.*(a*b);
-y=sigma_f.^2.*exp(-.5.*r2)
+y=sigma_f.^2.*exp(-.5.*r2)+sw_1*sigma_n2*eye(N1,N2);
 % y=sigma_f.^2.*exp(-.5.*(a-b)'*M*(a-b));
--- Gassian_regression_no_noise.m
+++ Gassian_regression_no_noise.m
@@ -1,27 +1,25 @@
-N=8;
-x=linspace(0,10,N)';
-y=sin(x);
-x_real=linspace(-1,11,20*N)';
-y_real=sin(x_real);
+N=20;N_test=61;sigma_n2=1e-4;LL=1;
+x = gpml_randn(0.8, N, 1);                 % 20 training inputs
+y = sin(3*x) + 0.1*gpml_randn(0.9, N, 1);  % 20 noisy training targets
+x_test = linspace(-3, 3, N_test)';                  % 61 test inputs 
 % plot(x,y);
-K=GPR_kernel(x,x');
-Ky=K+exp(-10).*eye(N);
+K=GPR_kernel(x,x',LL,sigma_n2);
+Ky=K;
 L=chol(Ky,'lower');
 % f_prior=L*randn(N,5);
 % figure
 % plot(x,f_prior,'LineWidth',1.5);
 
-N_test=79;
-x_test=linspace(-1,11,N_test)';
 alpha=L'\(L\y);
-K_test=GPR_kernel(x_test,x');
+K_test=GPR_kernel(x_test,x',LL,sigma_n2);
 f_posterior=K_test*alpha;
 v = L\K_test';
-K_test_test=GPR_kernel(x_test,x_test');
+K_test_test=GPR_kernel(x_test,x_test',LL,sigma_n2);
 var_f = K_test_test - v'*v;
-f_posterior_var=K_test*alpha+var_f*randn(N_test,5);
-f_posterior_var_range=K_test*alpha+var_f*ones(N_test,1)*[2 -2];
-figure
-fill([x_test; flip(x_test,1)], [f_posterior_var_range(:,1);flip(f_posterior_var_range(:,2),1)], [7 7 7]/8);
+L_var_f=chol(var_f,'lower');
+f_posterior_var=K_test*alpha+L_var_f*randn(N_test,3);
+f_posterior_var_range=K_test*alpha+L_var_f*ones(N_test,1)*[2 -2];
+% figure
+fill([x_test; flip(x_test,1)], [f_posterior_var_range(:,1);flip(f_posterior_var_range(:,2),1)], [7 7 7]/7.5);
 hold on; grid on
-plot(x_real,y_real,'r',x,y,'*',x_test,f_posterior,'.-',x_test,f_posterior_var,'--','LineWidth',1.5);
+plot(x,y,'o',x_test,f_posterior,'.-',x_test,f_posterior_var,'--','LineWidth',1.5);
